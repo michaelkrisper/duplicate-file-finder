3c34ef1623da9a24192d8a4fe1ecd79e19466dee
Michael Krisper <michael.krisper@tugraz.at>
2012-04-28 09:44:20 +0200

    ran pylint. updated variable names, and did some refactoring. added docstrings.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 duplicate-file-finder
 =====================
 
-A script to find duplicate files and display them on the screen
+A script to find duplicate files and display them on the screen.
+
+usage:  duplicatefilefinder.py <directory>
+
+Arguments:
+	<directory>	 the directory which should be checked for duplicate files.
+
+Example:
+	python duplicatefilefinder.py ~/Downloads
+	
+
+
+Description:
+=====================
+Shows the top 10 duplicate files. Does not check in hidden directories (directories with the prefix ".")
--- duplicatefilefinder.py
+++ duplicatefilefinder.py
@@ -1,68 +1,100 @@
 # -*- coding: UTF-8 -*-
+"""
+Functions for traversing a directory structure and finding duplicate files.
+This is done by hashing every single file and comparing the hashes.
+"""
 
 import sys
 import os
 import hashlib
 
-def findAllDuplicates(path, chunkSize=1024*20):
-    fileCounter = 0
+
+def get_hash(chunk_size, filepath):
+    """ Calculates the hash of a file. """
+    hash_object = hashlib.sha1()
+    with open(filepath, 'rb') as f_input:
+    # todo: better performance idea: hash in chunks of 1024, and only hash more if a duplicate was found.
+    # todo: make the chunk_size configureable!
+        for chunk in iter(lambda:f_input.read(chunk_size), ""):
+            hash_object.update(chunk)
+    
+    digest = hash_object.digest()
+    return digest
+
+def find_all_duplicates(path, chunk_size=1024 * 100):
+    """ Finds alle duplicate files in the path. """
+    file_counter = 0
     for dirpath, _, files in os.walk(path):
         # exclude "hidden" directories (dirs with . prefix, e.g. ~/.ssh)
         # todo: make that configureable!
         if "/." not in dirpath:
-            fileCounter = fileCounter + len(files)
-            print "Counting files ... %d\r" % fileCounter,
+            file_counter = file_counter + len(files)
+            print "Counting files ... %d\r" % file_counter,
     print ""
     
     hashes = {}    
-    fileCounter = 0
-    dupeCount = 0
+    file_counter = 0
+    dupe_count = 0
     for dirpath, _, filenames in os.walk(path):
         for filename in filenames:
             # exclude "hidden" directories (dirs with . prefix, e.g. ~/.ssh)
             # todo: make that configureable!
             if "/." not in dirpath:
                 filepath = os.path.join(dirpath, filename)
-                hashObject = hashlib.sha1()
-                with open(filepath, 'rb') as f:
-                    # todo: check performance idea: hash in chunks of 1024, and only hash more if a duplicate was found.
-                    # todo: make the chunkSize configureable!
-                    hashObject.update(f.read(chunkSize))
-                hashDigest = hashObject.digest()
-                if not hashes.has_key(hashDigest):
-                    hashes[hashDigest] = [filepath]
+                digest = get_hash(chunk_size, filepath)
+                if not hashes.has_key(digest):
+                    hashes[digest] = [filepath]
                 else:
-                    if len(hashes[hashDigest]) == 1: 
-                        dupeCount += 1
-                    hashes[hashDigest].append(filepath)
-                fileCounter += 1
-                print "Files checked: %d - Duplicates found: %d\r" % (fileCounter, dupeCount),
+                    if len(hashes[digest]) == 1: 
+                        dupe_count += 1
+                    hashes[digest].append(filepath)
+                file_counter += 1
+                print "Files checked: %d - Duplicates found: %d\r" % (file_counter, dupe_count),
+                sys.stdout.flush()
     return [filelist for filelist in hashes.values() if len(filelist) > 1]
 
-if sys.argv[1:]:
-    dupes = findAllDuplicates(sys.argv[1])
-            
-    conflictedFiles = []
-    for filePaths in dupes:
-        for filePath in filePaths:
-            if "conflicted" in filePath.lower():
-                conflictedFiles.append(filePaths)
-                break              
-    dupes = conflictedFiles
+def main(path):
+    """ The main method """
+    dupes = find_all_duplicates(path)
     
-    print "\n\nDisplaying Top 10 of most duplicated files:"
-    for pos, filePaths in enumerate(sorted(dupes, key=lambda filePaths: len(filePaths), reverse=True), start=1):
-        commonPrefix = os.path.dirname(os.path.commonprefix(filePaths))
-        print "\n(%d) Found %d duplicate files (size: %d Bytes) in %s/:" % (pos, 
-                                                                            len(filePaths),
-                                                                            os.path.getsize(filePaths[0]), 
-                                                                            commonPrefix)
-        for i, filePath in enumerate(filePaths, start=1):
-            print "\t%d: %s" % (i, filePath[len(commonPrefix)+1:])
+    # only show "dropbox" conflicted files            
+    #conflicted_files = []
+    #for paths in dupes:
+    #    for path in paths:
+    #        if "conflicted" in path.lower():
+    #            conflicted_files.append(paths)
+    #            break
+    #dupes = conflicted_files
+    
+    if len(dupes) > 0:
+        print "\n\nDisplaying Top 10 of most duplicated files:"
+        for pos, paths in enumerate(sorted(dupes, key=len, reverse=True)[:10], start=1):
+            prefix = os.path.dirname(os.path.commonprefix(paths))
+            print "\n(%d) Found %d duplicate files (size: %d Bytes) in %s/:" % (pos,
+                                                                                len(paths),
+                                                                                os.path.getsize(paths[0]),
+                                                                                prefix)
+            for i, path in enumerate(paths, start=1):
+                print "\t%d: %s" % (i, path[len(prefix) + 1:])
             
-    print "\nFound %d duplicates (%d duplicate files in total)" % (len(dupes), 
+    print "\nFound %d duplicates (%d duplicate files in total)" % (len(dupes),
                                                                    reduce(lambda sumValue, files: sumValue + len(files), dupes, 0))
-else:
-    print "usage:  %s <directory>\n" % sys.argv[0]
-    print "Arguments:"
-    print "<directory>\t the directory which should be checked for duplicate files.\n"
+
+
+
+if __name__ == "__main__":
+    if sys.argv[1:]:
+        #todo: write unit test!
+        #todo: document functions
+        #todo: add better argument check
+        #todo: add -h, --help command
+        #todo: add -v, --verbose for verbose mode (output all scanned files; when a duplicate was found; hashDigests too?)
+        #todo: add -s, --size for manual chunksize setting
+        #todo: add -a, --all for display of all duplicate files, not only the top10
+        main(sys.argv[1])
+        
+    else:
+        print "Searches for duplicate files and then shows the Top 10 of most duplicated files.\n"
+        print "usage:  %s <directory>\n" % os.path.basename(sys.argv[0])
+        print "Arguments:\n\t<directory>\t the directory which should be checked for duplicate files.\n"
+        print "Example:\n\tduplicatefilefinder.py ~/Downloads\n"
