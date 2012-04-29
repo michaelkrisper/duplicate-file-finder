3f26d2dcad86ff38a8810894862fd3570eb330b1
Michael Krisper <michael.krisper@tugraz.at>
2012-04-29 23:09:06 +0200

    shortened the source code

--- duplicatefilefinder.py
+++ duplicatefilefinder.py
@@ -32,24 +32,12 @@ def parse_arguments():
         Description: Searches duplicates and also include hidden or empty FILES"""
 
     parser = argparse.ArgumentParser(description=__doc__, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    parser.add_argument(dest="directory",
-                        help="the directory which should be checked for duplicate FILES")
-
+    parser.add_argument(dest="directory", help="the directory which should be checked for duplicate FILES")
     group = parser.add_mutually_exclusive_group()
-
-    group.add_argument("-a", dest="show_all", action="store_true",
-                       help="display all duplicate FILES. equal to -top 0")
-
-    group.add_argument("-top", dest="top", action="store", metavar="COUNT", type=int, default=10,
-                       help="set the amount of displayed duplicates. If 0 is given, all results will be displayed. default=10")
-
-    parser.add_argument("--hidden", dest="include_hidden", action="store_true",
-                        help="check hidden FILES and directories too")
-
-    parser.add_argument("--empty", dest="include_empty", action="store_true",
-                        help="check empty FILES too")
-
+    group.add_argument("-a", dest="show_all", action="store_true", help="display all duplicate FILES. equal to -top 0")
+    group.add_argument("-top", dest="top", action="store", metavar="COUNT", type=int, default=10, help="set the amount of displayed duplicates. If 0 is given, all results will be displayed. default=10")
+    parser.add_argument("--hidden", dest="include_hidden", action="store_true", help="check hidden FILES and directories too")
+    parser.add_argument("--empty", dest="include_empty", action="store_true", help="check empty FILES too")
     return parser.parse_args()
 
 def print_duplicates(files, displaycount=None):
@@ -64,37 +52,30 @@ def print_duplicates(files, displaycount=None):
     print "\nFound %d duplicates (%d duplicate files total)" % \
         (len(files), reduce(lambda sumValue, files: sumValue + len(files), files, 0))
 
-def filter_duplicate_files(fileList):
+def get_hash_key(filename):
+    """Calculates the hash value for a file."""
+    hash_object = md5.md5()
+    with open(filename, 'rb') as inputfile:
+        for chunk in iter(lambda:inputfile.read(1024 * 4), ""):
+            hash_object.update(chunk)
+    return hash_object.digest()
+
+def filter_duplicate_files(files):
     """Finds all duplicate files in the directory."""
     duplicates = {}
-    count = 0
-    for i, filepath in enumerate(fileList, start=1):
-        key = os.path.getsize(filepath)
-        duplicates.setdefault(key, []).append(filepath)
-        if len(duplicates[key]) > 1:
-            count += 1
-            if len(duplicates[key]) == 2:
+    for keyfunction, name in [(os.path.getsize, "By File Size"), (get_hash_key, "By File Hash")]:
+        duplicates.clear()
+        count = 0
+        for i, filepath in enumerate(files, start=1):
+            key = keyfunction(filepath)
+            duplicates.setdefault(key, []).append(filepath)
+            if len(duplicates[key]) > 1:
                 count += 1
-        print "\r(By File Size) %d Files checked, %d duplicates found  (%d duplicate files)" % (i, len(duplicates), count),
-    print ""
-    
-    fileList = (filepath for filepaths in duplicates.itervalues() if len(filepaths) > 1 for filepath in filepaths)
-    
-    duplicates = {}
-    count = 0
-    for i, filepath in enumerate(fileList, start=1):
-        hash_object = md5.md5()
-        with open(filepath, 'rb') as inputfile:
-            for chunk in iter(lambda:inputfile.read(1024 * 4), ""):
-                hash_object.update(chunk)
-        key = hash_object.digest()
-        duplicates.setdefault(key, []).append(filepath)
-        if len(duplicates[key]) > 1:
-            count += 1
-            if len(duplicates[key]) == 2:
-                count += 1
-        print "\r(By File Hash) %d Files checked, %d duplicates found  (%d duplicate files)" % (i, len(duplicates), count),
-    print ""
+                if len(duplicates[key]) == 2:
+                    count += 1
+            print "\r(%s) %d Files checked, %d duplicates found (%d duplicate files)" % (name, i, len(duplicates), count),
+        print ""
+        files = [filepath for filepaths in duplicates.itervalues() if len(filepaths) > 1 for filepath in filepaths]
 
     return [filelist for filelist in duplicates.itervalues() if len(filelist) > 1]
 
@@ -107,7 +88,6 @@ def get_files(directory, include_hidden, include_empty):
                 and (include_hidden or
                      reduce(lambda r, d: r and not d.startswith("."), os.path.join(dirpath, filename).split(os.sep)[1:], True))
                 and (include_empty or os.path.getsize(os.path.join(dirpath, filename)) > 0))
-    
 
 if __name__ == "__main__":
     ARGS = parse_arguments()
