fe4df1810d73c7393f8e1614b040b4def79753ec
Michael Krisper <michael.krisper@tugraz.at>
2012-05-01 00:37:20 +0200

    added docstrings and change variable names (pylint is happy now)

--- UpdatePrinter.py
+++ UpdatePrinter.py
@@ -2,12 +2,15 @@ import sys
 import time
 
 class UpdatePrinter(object):
+    """Class for printing nice status output on the console."""
     def __init__(self, refreshrate=0.05):
         self.__last = 0
         self.__last_text_length = 0
         self.refreshrate = refreshrate
 
     def update(self, value, force=False, flush=True):
+        """Updates the last line on the console. Overwrites previous output made with 
+        this method. Has a mechanism which prevents flickering. Use the force parameter to enforce output."""
         if ((time.time() - self.__last) >= self.refreshrate) or force:
             print "\r%s%s" % (value, " " * (self.__last_text_length - len(value))),
             self.__last_text_length = len(value)
@@ -16,11 +19,12 @@ class UpdatePrinter(object):
             self.__last = time.time()
 
 if __name__ == "__main__":
-    printer = UpdatePrinter()
+    PRINTER = UpdatePrinter()
     print "This is the NicePrinter."
+    i = 1
     for i in range(50, 0, -1):
-        printer.update("Printing a's from 50 to 1: %s" % ("a" * i))
+        PRINTER.update("Printing a's from 50 to 1: %s" % ("a" * i))
         time.sleep(0.1)
     else:
-        printer.update("Printing a's from 50 to 1: %s" % ("a" * i), force=True)
+        PRINTER.update("Printing a's from 50 to 1: %s" % ("a" * i), force=True)
     print "\nAnd this is the next output!"
